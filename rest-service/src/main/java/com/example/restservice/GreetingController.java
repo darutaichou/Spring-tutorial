e9a9b91bae7ed8a58d975fb90f41635bd764b17a
dahi0104 <adgjmptw12behkilorvy@gmail.com>
2021-02-22 22:32:58 +0900

    Greetingコントローラ作成

--- rest-service/src/main/java/com/example/restservice/GreetingController.java
+++ rest-service/src/main/java/com/example/restservice/GreetingController.java
@@ -1,5 +1,30 @@
 package com.example.restservice;
 
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+// HTTPリクエストのidとリクエストで指定されたnameパラメータを設定したGreetingオブジェクトを画面に表示する
+
+@RestController
 public class GreetingController {
 
+	// idを一意にするためのカウンター
+	AtomicLong counter = new AtomicLong();
+	// %sにはリクエストパラメータのnameの値が入る(パラメータがない場合は"World"が入る)
+	String content = "Hello %s";
+
+	// メイン処理
+	@GetMapping("/rest-service")
+	public Greeting hello(@RequestParam(value = "name", defaultValue = "World")String name) {
+
+		// Greetingクラスのコンストラクタに一意のidと、あいさつ文をセット
+		Greeting greet = new Greeting((int)counter.incrementAndGet(), String.format(content,name));
+
+		return greet;
+
+
+	}
 }
